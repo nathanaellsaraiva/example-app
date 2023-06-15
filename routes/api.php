c52a16526e785d98cc8565741f34d4a26edc1f29
Nathanael <natham_11@hotmail.com>
2023-06-15 19:07:06 -0300

    feat: Utilizando Laravel Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +18,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::post("/register", function(Request $request) {
+    $request->validate([
+        "email" => "required|string|email|unique:users",
+        "name" => "required|string",
+        "password" => "required|string|min:6"
+    ]);
+
+    $user = User::create([
+        "email" => $request->email,
+        "name" => $request->name,
+        "password" => Hash::make($request->password)
+    ]);
+
+    return response()->json([
+        "message" => "Sucesso",
+        "user" => $user
+    ]);
+});
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only('email', 'password');
+
+    if (Auth::attempt($credentials)) {
+        $user = $request->user();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            "access_token" => $token,
+            "token_type" => 'Bearer'
+        ]);
+    }
+
+    return response()->json([
+        "message" => "Usuário inválido"
+    ]);
+});
+
+Route::middleware('auth:sanctum')->get('/user/profile', function (Request $request) {
     return $request->user();
 });
